0bb4ea30ca3eaa01d9cda06eac0aa5bb289d7cba
Recklessrage17 <masterjrgrove.jg@gmail.com>
2026-06-11 06:51:27 -0500

    fix

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App, { renderStandalonePrintableReportIfRequested } from "./App";
+import App from "./App";
 import { runDevSqliteRuntimeCheck } from "./lib/sqliteRuntime";
 import "./styles.css";
 
-if (!renderStandalonePrintableReportIfRequested()) {
-  void runDevSqliteRuntimeCheck();
+void runDevSqliteRuntimeCheck();
 
-  ReactDOM.createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-}
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
